a666675fc78a8452564c2cd5f3bcc06bb63f9d61
DaGuest <daangast@gmail.com>
2020-04-20 15:16:37 +0200

    Switch to if statement
    changed switch case to if else statements

--- Assets/Scripts/BehaviourTrigger.cs
+++ Assets/Scripts/BehaviourTrigger.cs
@@ -6,24 +6,20 @@ public class BehaviourTrigger : MonoBehaviour {
 	public int triggerBehaviour = 0;
 
 	void OnTriggerEnter2D(Collider2D other) {
-		switch (triggerBehaviour) {
-			case 0:
-				other.GetComponent<Player>().SetPoepAble(true);
-				break;
-			case 1:
-				other.GetComponent<Player>().SetGamen(true);
-				break;
+		if (triggerBehaviour == 0){
+			other.GetComponent<Player>().SetPoepAble(true);
+		}
+		else if (triggerBehaviour == 1) {
+			other.GetComponent<Player>().SetGamen(true);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
-		switch (triggerBehaviour) {
-			case 0:
-				other.GetComponent<Player>().SetPoepAble(false);
-				break;
-			case 1:
-				other.GetComponent<Player>().SetGamen(false);
-				break;
+		if (triggerBehaviour == 0){
+			other.GetComponent<Player>().SetPoepAble(false);
+		}
+		else if (triggerBehaviour == 1) {
+			other.GetComponent<Player>().SetGamen(false);
 		}
 	}
 }
